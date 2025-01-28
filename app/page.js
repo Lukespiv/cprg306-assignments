35d332245904a99ac4c971bb0f6f82cc68170f8a
Luke <luke.spivak@edu.sait.ca>
2025-01-28 16:36:09 -0700

    First commit

---
+++ app/page.js
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default function Page() {
+  return (
+    <main>
+      <h1>CPRG 360: Web Development 2 - Assignments</h1>
+      <p>
+        <a href="/week-2">Week 2 Assignment</a>
+      </p>
+    </main>
+
+  );
+}
---
+++ app/week-2/student-info.js
@@ -0,0 +1,12 @@
+export default function StudentInfo()
+{
+
+   return (
+    <div>
+        <h2>Luke Spivak</h2>
+        <p>
+            <a href="https://github.com/Lukespiv?tab=repositories" target= "_blank" rel="noopener noreferrer">https://github.com/ </a> 
+        </p>
+    </div>
+    );
+};
