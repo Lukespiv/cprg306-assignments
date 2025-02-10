995cc09bdfe717115149cfc2c1a7ee7cfab8c6a0
Luke <luke.spivak@edu.sait.ca>
2025-02-10 15:55:28 -0700

    week-4

--- app/page.js
+++ app/page.js
@@ -1,11 +1,15 @@
-import Image from "next/image";
-
 export default function Page() {
   return (
     <main>
       <h1>CPRG 360: Web Development 2 - Assignments</h1>
       <p>
         <a href="/week-2">Week 2 Assignment</a>
+      </p>
+      <p>
+        <a href="/week-3">Week 3 Assignment</a>
+      </p>
+      <p>
+        <a href="/week-4">Week 4 Assignment</a>
       </p>
     </main>
 
