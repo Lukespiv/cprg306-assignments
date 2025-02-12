80edd650c690ba9d1684ad7d121f66cb2fa29feb
Luke <luke.spivak@edu.sait.ca>
2025-02-11 23:52:04 -0700

    week-4

--- app/page.js
+++ app/page.js
@@ -1,17 +1,17 @@
+import Link from "next/link";
+
 export default function Page() {
   return (
+    <div className="items-center justify-items-center">
     <main>
       <h1>CPRG 360: Web Development 2 - Assignments</h1>
-      <p>
-        <a href="/week-2">Week 2 Assignment</a>
-      </p>
-      <p>
-        <a href="/week-3">Week 3 Assignment</a>
-      </p>
-      <p>
-        <a href="/week-4">Week 4 Assignment</a>
-      </p>
+      <div className="flex flex-col gap-2">
+        <Link href="/week-2" className="text-green-500 underline text-lg">week-2</Link>
+        <Link href="/week-3" className="text-green-500 underline text-lg">week-3</Link>
+        <Link href="/week-4" className="text-green-500 underline text-lg">week-4</Link>
+      </div>
     </main>
+    </div>
 
   );
 }
