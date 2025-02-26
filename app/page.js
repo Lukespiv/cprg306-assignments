d82c7f72ff9cae6b3ceec68049838ec21faaa4bd
Luke <luke.spivak@edu.sait.ca>
2025-02-26 16:38:06 -0700

    changed

--- app/page.js
+++ app/page.js
@@ -3,13 +3,14 @@ import Link from "next/link";
 export default function Page() {
   return (
     <div className="items-center justify-items-center">
-    <main>
-      <h1>CPRG 360: Web Development 2 - Assignments</h1>
-      <div className="flex flex-col gap-2">
+    <main >
+      <h1 className="text-xl font-bold text-center mb-4">CPRG 360: Web Development 2 - Assignments</h1>
+      <div className="flex flex-col gap-2 items-center">
         <Link href="/week-2" className="text-green-500 underline text-lg">week-2</Link>
         <Link href="/week-3" className="text-green-500 underline text-lg">week-3</Link>
         <Link href="/week-4" className="text-green-500 underline text-lg">week-4</Link>
         <Link href="/week-5" className="text-green-500 underline text-lg">week-5</Link>
+        <Link href="/week-6" className="text-green-500 underline text-lg">week-6</Link>
       </div>
     </main>
     </div>
--- app/week-2/student-info.js
+++ app/week-2/student-info.js
@@ -3,8 +3,8 @@ export default function StudentInfo()
 
    return (
     <div>
-        <h2>Luke Spivak</h2>
-        <p>
+        <h2 className="text-xl font-bold">Luke Spivak</h2>
+        <p className="text-green-500 underline text-lg">
             <a href="https://github.com/Lukespiv?tab=repositories" target= "_blank" rel="noopener noreferrer">https://github.com/ </a> 
         </p>
     </div>
