8c022160a79177a9123a3f2c296590569029b160
Luke <luke.spivak@edu.sait.ca>
2025-03-07 16:33:16 -0700

    week-7

---
+++ app/week-7/item-list.js
@@ -0,0 +1,38 @@
+'use client';
+
+import Item from "./item";
+import {useState} from "react";
+
+export function ItemList({ items })
+{
+  const [sortBy, setSortBy] = useState("name");
+
+  const sortedItems = [...items].sort((a, b) => {
+    if (sortBy === 'name') {
+      return a.name.localeCompare(b.name);
+    } else {
+      return a.category.localeCompare(b.category);
+    }
+});
+
+return(
+  <div>
+    <button onClick={() => setSortBy("name")} 
+      className={`px-4 py-2 rounded-lg transition-all m-2 ${sortBy == "name" ? "bg-blue-500 text-black shadow-md": "bg-red-500 text-black"}`}> Name</button>
+
+    <button onClick={() => setSortBy("category")}
+      className={`px-4 py-2 rounded-lg transition-all m-2 ${sortBy == "category" ? "bg-blue-500 text-black shadow-md": "bg-red-500 text-black"}`}> Category</button>
+
+      <ul>
+      {sortedItems.map((item) => (
+          <Item
+            key={item}
+            name={item.name}
+            quantity={item.quantity}
+            category={item.category}
+          />
+        ))}
+      </ul>
+  </div>
+)
+}
