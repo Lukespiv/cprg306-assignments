2d0007578b96ae6b4b27c1ef2aabf733c69fc2ac
Luke <luke.spivak@edu.sait.ca>
2025-03-11 15:33:51 -0600

    week-7

--- app/page.js
+++ app/page.js
@@ -11,6 +11,7 @@ export default function Page() {
         <Link href="/week-4" className="text-green-500 underline text-lg">week-4</Link>
         <Link href="/week-5" className="text-green-500 underline text-lg">week-5</Link>
         <Link href="/week-6" className="text-green-500 underline text-lg">week-6</Link>
+        <Link href="/week-7" className="text-green-500 underline text-lg">week-7</Link>
       </div>
     </main>
     </div>
